bc3c7e8b4ff96f89dd238dbb01639294b3eabcff
huseyinnatess <ateshuseyin247@gmail.com>
2024-11-18 04:18:16 +0300

    ScoreTrigger'ına çift çarpma hatası düzeltildi.

--- Assets/Scripts/Runtime/Observers/ScoreObserver.cs
+++ Assets/Scripts/Runtime/Observers/ScoreObserver.cs
@@ -7,11 +7,15 @@ namespace Runtime.Observers
     public class ScoreObserver : IPlayerTriggerObserver
     {
         private ushort _score = 0;
-
+        private string _oldParentName;
         public void OnPlayerTriggered(Collider2D collider)
         {
-            if (collider.CompareTag("ScoreTrigger"))
+            string parentName = collider.transform.parent.name;
+            if (collider.CompareTag("ScoreTrigger") && _oldParentName != parentName)
+            {
+                _oldParentName = parentName;
                 UISignals.Instance.onScoreChange?.Invoke(++_score);
+            }
         }
     }
 }
